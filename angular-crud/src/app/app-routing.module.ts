de0d469db76312393efd1d76ba396757f50da78f
Mcjos15 <63763240+Mcjos15@users.noreply.github.com>
2022-05-04 11:19:00 -0600

    Cambio de rutas

--- angular-crud/src/app/app-routing.module.ts
+++ angular-crud/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'elementos',
+    path: '',
     loadChildren: () => import('./elementos/elementos.module').then(m => m.ElementosModule)
   }
 ];
